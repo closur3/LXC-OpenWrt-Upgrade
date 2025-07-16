e880dd6ffab0767b046de8dc8e68263754618926
hsux <72607204+closur3@users.noreply.github.com>
2025-07-16 23:27:14 +0800

    Update lxc.sh

--- lxc.sh
+++ lxc.sh
@@ -29,7 +29,6 @@ for cmd in pct qm wget awk grep sort uniq ping; do
     check_command "$cmd"
 done
 
-# 检测配置文件是否存在，否则生成
 INI_FILE="$(dirname "$0")/lxc.sh.ini"
 if [ ! -f "$INI_FILE" ]; then
     cat > "$INI_FILE" <<EOF
@@ -210,31 +209,39 @@ else
     log "下载成功"
 fi
 
-# 创建备份并从旧容器中拉取备份
+# 创建备份
 if [ "$backup_enabled" = "1" ]; then
     log "创建备份并从旧容器中拉取备份..."
     pct exec $old_container_id -- sysupgrade -b "$backup_file"
     check_result $? "创建备份失败。"
     pct pull $old_container_id "$backup_file" ~/backup.tar.gz
     check_result $? "从容器中拉取备份失败。"
+else
+    log "未启用备份，将跳过备份步骤。"
 fi
+
+# 预创建新容器
+log "预创建新容器..."
+pct create $new_container_id "$template" \
+    --rootfs "$rootfs" --ostype "$ostype" --hostname "$hostname" --arch "$arch" \
+    --cores "$cores" --memory "$memory" --swap "$swap" --onboot "$onboot" \
+    --startup "$startup" --features "$features" --net0 "$net0"
+check_result $? "创建新容器失败。"
 
 # 停止旧容器
 log "停止旧容器..."
 pct stop $old_container_id
 check_result $? "停止旧容器失败。"
 
-# 创建新容器
-log "创建新容器..."
-pct create $new_container_id "$template" --rootfs "$rootfs" --ostype "$ostype" --hostname "$hostname" --arch "$arch" --cores "$cores" --memory "$memory" --swap "$swap" --onboot "$onboot" --startup "$startup" --features "$features" --net0 "$net0"
-check_result $? "创建新容器失败。"
+# 启动新容器
+log "启动新容器..."
 pct start $new_container_id
 check_result $? "启动新容器失败。"
 sleep 3
 
 # 还原备份
 if [ "$backup_enabled" = "1" ]; then
-    log "将备份推送至新容器并还原备份..."
+    log "在新容器中还原备份..."
     pct push $new_container_id ~/backup.tar.gz "$backup_file"
     check_result $? "将备份推送到新容器失败。"
     pct exec $new_container_id -- sysupgrade -r "$backup_file"
